631c61697c4bb2e1fc7d95de3e1ac6c53e4f4cac
nithig <154483033+nithig@users.noreply.github.com>
2025-05-25 18:14:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function insert_Row() {
 	let element = document.getElementById("sampleTable");
-	element.innerHTML = "<tr><td>New Cell1Row1 cell1</td><td>New Cell2Row1 cell2</td></tr>" + element.innerHTML;
+	element.innerHTML = "<tr><td>New Cell1</td><td>New Cell2</td></tr>" + element.innerHTML;
 }
